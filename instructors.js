806b015bdd584ad9694ff12df72a4c8f2b436807
Sara Gomes <sarah.gomes80@hotmail.com>
2020-01-07 13:36:04 -0300

    Salvando e tratando dados do formulário

--- instructors.js
+++ instructors.js
@@ -1,22 +1,43 @@
-// create
+const fs = require('fs')
+const data = require("./data.json")
+
 exports.post = function(req,res) {
-    //req.query
-
-    //req.body
-    // {"avatar_url": "https://www.google.com/","name": "Sara Gomes do Nascimento Santana","birth": "225549-05-22","gender": "M","services": ""}
     
-
-    // ["avatar_url","name","birth","gender","services"]
     const keys = Object.keys(req.body)
 
     for(key of keys) {
-        // req.body.key == ""
+        
         if(req.body[key] == "") {
             return res.send("Por favor, preencha todos os campos!")
         }
             
     }
 
-    return res.send(req.body)
+    let { avatar_url, birth, name, services, gender } = req.body
+
+    
+    birth = Date.parse(birth) 
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+
+    
+    data.instructors.push({ 
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    }) 
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Write file error")
+
+        return res.redirect("/instructors")
+    })
+
+    // return res.send(req.body)
 }
 
