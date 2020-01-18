14248b7c03bd121f5d2f438250eefd95f154f2ff
Sara Gomes <sarah.gomes80@hotmail.com>
2020-01-18 12:17:04 -0300

    Aplicando a lógica de Menu Ativo

--- public/scripts.js
+++ public/scripts.js
@@ -1,2 +1,8 @@
 const currentPage = location.pathname
-console.log(currentPage)
+const menuItems = document.querySelectorAll("header .links a")
+
+for (item of menuItems) {
+    if (currentPage.includes(item.getAttribute("href"))) {
+        item.classList.add("active")
+    }
+}
