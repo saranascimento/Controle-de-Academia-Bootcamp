20e6ef99ec11998283b9b5f7cbe377d0bfcc27d9
Sara Gomes <sarah.gomes80@hotmail.com>
2020-01-19 11:49:36 -0300

    Salvando e apresentando os dados dos membros

--- controllers/members.js
+++ controllers/members.js
@@ -3,10 +3,53 @@ const data = require('../data.json')
 const { age, date } = require('../utils')
 const Intl = require('intl')
 
+// list data
 exports.index = function(req, res) {
     return res.render("members/index", {members: data.members})
 }
 
+// create data
+exports.create = function(req, res) {
+    return res.render("members/create")
+}
+
+// receives data
+exports.post = function(req, res) {
+    
+    const keys = Object.keys(req.body)
+
+    for(key of keys) {
+        
+        if(req.body[key] == "") {
+            return res.send("Por favor, preencha todos os campos!")
+        }
+                
+    }
+
+    birth = Date.parse(req.body.birth) 
+
+    let id = 1
+    const lastMember = data.members[data.members.length - 1]
+
+    if (lastMember) {
+        id = lastMember.id + 1
+    } 
+
+    data.members.push({ 
+        id,
+        ...req.body, 
+        birth 
+    }) 
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Write file error")
+
+        return res.redirect(`/members/${id}`)
+    })
+
+}
+
+// shows data
 exports.show = function(req, res) {
     // req.params
     const { id } = req.params
@@ -26,48 +69,7 @@ exports.show = function(req, res) {
     return res.render("members/show", { member })
 }
 
-exports.create = function(req, res) {
-    return res.render("members/create")
-}
-
-exports.post = function(req, res) {
-    
-    const keys = Object.keys(req.body)
-
-    for(key of keys) {
-        
-        if(req.body[key] == "") {
-            return res.send("Por favor, preencha todos os campos!")
-        }
-                
-    }
-
-    let { avatar_url, birth, name, services, gender } = req.body
-
-    
-    birth = Date.parse(birth) 
-    const created_at = Date.now()
-    const id = Number(data.members.length + 1)
-
-    data.members.push({ 
-        id,
-        avatar_url,
-        name,
-        birth,
-        gender,
-        services,
-        created_at
-    }) 
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-        if(err) return res.send("Write file error")
-
-        return res.redirect(`/members/${id}`)
-    })
-
-    // return res.send(req.body)
-}
-
+// edit data
 exports.edit = function(req, res) {
     // req.params
     const { id } = req.params
@@ -87,6 +89,7 @@ exports.edit = function(req, res) {
     return res.render('members/edit', { member })
 }
 
+// update  data
 exports.put = function(req, res) {
     // req.body
     const { id } = req.body
@@ -117,6 +120,7 @@ exports.put = function(req, res) {
     })
 }
 
+// delete data
 exports.delete = function(req, res) {
     const { id } = req.body
 
