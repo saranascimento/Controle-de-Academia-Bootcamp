32653f8b7653f7fadf23fa32c643cec0ee593ad3
Sara Gomes <sarah.gomes80@hotmail.com>
2020-01-19 14:49:31 -0300

    Lógica de confirmação ao deletar membro/instrutor

--- public/scripts.js
+++ public/scripts.js
@@ -6,3 +6,11 @@ for (item of menuItems) {
         item.classList.add("active")
     }
 }
+
+const formDelete = document.querySelector("#form-delete")
+    formDelete.addEventListener("submit", function(event) {
+        const confirmation = confirm("Deseja Deletar?")
+        if(!confirmation) {
+            event.preventDefault()
+        }
+})
