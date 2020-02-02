d7dadfa0d6d12d7b5c324c01d1ef311a76de7b53
Sara Gomes <sarah.gomes80@hotmail.com>
2020-02-02 16:22:40 -0300

    Montando a paginação no Front

--- public/scripts.js
+++ public/scripts.js
@@ -7,37 +7,50 @@ for (item of menuItems) {
     }
 }
 
-const formDelete = document.querySelector("#form-delete")
-    formDelete.addEventListener("submit", function(event) {
-        const confirmation = confirm("Deseja Deletar?")
-        if(!confirmation) {
-            event.preventDefault()
+function paginate(selectedPage, totalPages) {
+    let pages = [],
+        oldPage
+
+    for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+        const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+        const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+        if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+            
+
+            if (oldPage && currentPage - oldPage > 2){
+                pages.push("...")
+            }
+
+            if(oldPage && currentPage - oldPage == 2) {
+                pages.push(oldPage + 1)
+            }
+            pages.push(currentPage)
+
+            oldPage = currentPage
         }
-})
-
-let totalPage = 20,
-    selectedPage = 5,
-    pages = [],
-    oldPage
-
-for(let currentPage = 1; currentPage <= totalPage; currentPage++) {
-
-    const firstAndLastPage = currentPage == 1 || currentPage == totalPage
-    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
-    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
-
-    if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
-        pages.push(currentPage)
-
-        if (oldPage && currentPage - oldPage > 2){
-            pages.push("...")
-        }
-
-        if(oldPage && currentPage - oldPage == 2) {
-            pages.push(oldPage + 1)
-        }
-
-        oldPage = currentPage
     }
+    return pages
 }
-console.log(pages)
+
+const pagination = document.querySelector(".pagination")
+const page = +pagination.dataset.page
+const total = +pagination.dataset.total
+const pages = paginate(page, total)
+
+
+let elements = ""
+
+for (let page of pages) {
+    if(String(page).includes("...")) {
+        elements += `<span>${page}</span>`
+    } else {
+        elements += `<a href="?page=${page}">${page}</a>`
+    }
+    
+}
+
+pagination.innerHTML = elements
+
